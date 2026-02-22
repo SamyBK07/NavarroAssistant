69e7cbc7b49d17911313eb53b01af570e58e8312
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-22 16:53:16 +0100

    Update PermissionsHelper.kt

--- app/src/main/java/com/navarro/utils/PermissionsHelper.kt
+++ app/src/main/java/com/navarro/utils/PermissionsHelper.kt
@@ -1,1 +1,23 @@
+package com.navarro.utils
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+object PermissionsHelper {
+    fun checkAndRequestPermission(
+        activity: Activity,
+        permission: String,
+        requestCode: Int,
+        onGranted: () -> Unit
+    ) {
+        if (ContextCompat.checkSelfPermission(activity, permission) ==
+            PackageManager.PERMISSION_GRANTED) {
+            onGranted()
+        } else {
+            ActivityCompat.requestPermissions(activity, arrayOf(permission), requestCode)
+        }
+    }
+}
 
