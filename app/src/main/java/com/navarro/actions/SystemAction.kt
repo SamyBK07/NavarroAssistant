573124a5abbf25c3f9ec0c6742e70277c51f3f14
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-22 01:49:35 +0100

    Update SystemAction.kt

--- app/src/main/java/com/navarro/actions/SystemAction.kt
+++ app/src/main/java/com/navarro/actions/SystemAction.kt
@@ -1,1 +1,12 @@
+package com.navarro.actions
 
+import android.content.Context
+import com.navarro.core.Logger
+
+class SystemAction(private val contexte: Context) {
+
+    fun executer(commande: String) {
+        Logger.i("SystemAction: exécution pour -> $commande")
+        // TODO: Intégrer commandes système (mode silencieux, volume, etc.)
+    }
+}
