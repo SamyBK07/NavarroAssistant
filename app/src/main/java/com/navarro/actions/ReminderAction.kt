656ac273608ccdaf270abcce08912e460748e83e
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-22 01:48:46 +0100

    Update ReminderAction.kt

--- app/src/main/java/com/navarro/actions/ReminderAction.kt
+++ app/src/main/java/com/navarro/actions/ReminderAction.kt
@@ -1,1 +1,12 @@
+package com.navarro.actions
 
+import android.content.Context
+import com.navarro.core.Logger
+
+class ReminderAction(private val contexte: Context) {
+
+    fun executer(commande: String) {
+        Logger.i("ReminderAction: exécution pour -> $commande")
+        // TODO: Intégrer création de rappel ou alarme
+    }
+}
