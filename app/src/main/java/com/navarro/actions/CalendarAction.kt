d91b25e0af9650b8c493c0150ab1893f4cbc2951
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-22 16:47:36 +0100

    Update CalendarAction.kt

--- app/src/main/java/com/navarro/actions/CalendarAction.kt
+++ app/src/main/java/com/navarro/actions/CalendarAction.kt
@@ -1,12 +1,8 @@
 package com.navarro.actions
 
-import android.content.Context
-import com.navarro.core.Logger
-
-class CalendarAction(private val contexte: Context) {
-
-    fun executer(commande: String) {
-        Logger.i("CalendarAction: exécution pour -> $commande")
-        // TODO: Intégrer accès agenda Android
+class CalendarAction : BaseAction() {
+    override fun execute(command: String): String {
+        // TODO: Intégration avec le calendrier du téléphone
+        return "Action calendrier pour: $command (non implémentée)"
     }
 }
