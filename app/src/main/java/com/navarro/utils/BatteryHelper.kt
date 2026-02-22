128966053093776f1957e1270b65e9f2ffcd5bc1
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-22 16:52:45 +0100

    Update BatteryHelper.kt

--- app/src/main/java/com/navarro/utils/BatteryHelper.kt
+++ app/src/main/java/com/navarro/utils/BatteryHelper.kt
@@ -1,1 +1,11 @@
+package com.navarro.utils
 
+import android.content.Context
+import android.os.BatteryManager
+
+object BatteryHelper {
+    fun getBatteryLevel(context: Context): Int {
+        val batteryManager = context.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+        return batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+    }
+}
