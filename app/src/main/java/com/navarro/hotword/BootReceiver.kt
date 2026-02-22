3a456eea06ba1674ff6d0752003eaec4a8cd8a8e
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-22 01:03:18 +0100

    Update BootReceiver.kt

--- app/src/main/java/com/navarro/hotword/BootReceiver.kt
+++ app/src/main/java/com/navarro/hotword/BootReceiver.kt
@@ -1,1 +1,15 @@
+package com.navarro.hotword
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+            val serviceIntent = Intent(context, HotwordService::class.java)
+            context.startForegroundService(serviceIntent)
+        }
+    }
+}
