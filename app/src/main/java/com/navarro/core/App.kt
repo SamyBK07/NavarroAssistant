0d158f44e779d6a6dd8b120006ef7f53d0235d7d
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-22 01:28:27 +0100

    Update App.kt

--- app/src/main/java/com/navarro/core/App.kt
+++ app/src/main/java/com/navarro/core/App.kt
@@ -1,1 +1,37 @@
+package com.navarro.core
 
+import android.app.Application
+import android.content.Context
+import com.navarro.core.AppConfig.MODELS_DIR
+import java.io.File
+
+class App : Application() {
+
+    companion object {
+        lateinit var instance: App
+            private set
+
+        fun appContext(): Context = instance.applicationContext
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        instance = this
+
+        Logger.i("Application started")
+
+        initModelDirectory()
+    }
+
+    private fun initModelDirectory() {
+        val modelsDir = File(filesDir, MODELS_DIR)
+
+        if (!modelsDir.exists()) {
+            val created = modelsDir.mkdirs()
+            Logger.i("Models directory created: $created")
+        } else {
+            Logger.i("Models directory exists")
+        }
+    }
+}
