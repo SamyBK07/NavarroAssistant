b86b7bb766d8f16b5960363ae236e7d491d77d99
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-22 01:47:56 +0100

    Update MessageAction.kt

--- app/src/main/java/com/navarro/actions/MessageAction.kt
+++ app/src/main/java/com/navarro/actions/MessageAction.kt
@@ -1,1 +1,12 @@
+package com.navarro.actions
 
+import android.content.Context
+import com.navarro.core.Logger
+
+class MessageAction(private val contexte: Context) {
+
+    fun executer(commande: String) {
+        Logger.i("MessageAction: exécution pour -> $commande")
+        // TODO: Intégrer envoi SMS ou WhatsApp
+    }
+}
