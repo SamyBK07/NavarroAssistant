6ef19d7a4527129e16c34ac79c2dde6c8930fc85
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-22 15:25:51 +0100

    Update MainActivity.kt

--- app/src/main/java/com/navarro/ui/MainActivity.kt
+++ app/src/main/java/com/navarro/ui/MainActivity.kt
@@ -1,1 +1,25 @@
+package com.navarro.ui
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.navarro.R
+import com.navarro.hotword.HotwordService
+import android.content.Intent
+import com.navarro.core.AppConfig
+import com.navarro.core.Logger
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Logger.i("MainActivity démarrée")
+
+        // Lancement du service Hotword au démarrage de l'activité
+        val intent = Intent(this, HotwordService::class.java).apply {
+            action = AppConfig.ACTION_START_HOTWORD
+        }
+        startForegroundService(intent)
+    }
+}
