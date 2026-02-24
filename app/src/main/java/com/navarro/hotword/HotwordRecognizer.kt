9dd4edb0c5177cebd5a78b8cc085825704c62166
SamyBK07 <kpeigounousamiou@gmail.com>
2026-02-24 03:44:13 +0100

    Update HotwordRecognizer.kt

--- app/src/main/java/com/navarro/hotword/HotwordRecognizer.kt
+++ app/src/main/java/com/navarro/hotword/HotwordRecognizer.kt
@@ -2,8 +2,6 @@ package com.navarro.hotword
 
 import android.content.Context
 import com.navarro.core.Logger
-import org.vosk.LibVosk
-import org.vosk.LogLevel
 import org.vosk.Model
 import org.vosk.Recognizer
 import org.vosk.android.RecognitionListener
@@ -18,14 +16,6 @@ class HotwordRecognizer(
     private var recognizer: Recognizer? = null
     private val audioBuffer = ShortArray(4096)
     private var isListening = false
-
-    init {
-        try {
-            LibVosk.setLogLevel(LogLevel.ERROR)
-        } catch (e: Exception) {
-            Logger.e("Vosk log level error: ${e.message}")
-        }
-    }
 
     fun startListening() {
         if (isListening) return
