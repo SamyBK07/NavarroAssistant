d26d4c2deed8d47b636dbfe97f4920706510d6c3
SamyBK07 <kpeigounousamiou@gmail.com>
2026-03-21 21:44:20 +0100

    Update Logger.kt

--- app/src/main/java/com/navarro/core/Logger.kt
+++ app/src/main/java/com/navarro/core/Logger.kt
@@ -3,9 +3,22 @@ package com.navarro.core
 import android.util.Log
 
 object Logger {
+
     private const val TAG = "NavarroAssistant"
 
-    fun d(message: String) = Log.d(TAG, message)
-    fun e(message: String) = Log.e(TAG, message)
-    fun i(message: String) = Log.i(TAG, message)
+    fun d(message: String) {
+        Log.d(TAG, message)
+    }
+
+    fun e(message: String) {
+        Log.e(TAG, message)
+    }
+
+    fun w(message: String) {
+        Log.w(TAG, message)
+    }
+
+    fun i(message: String) {
+        Log.i(TAG, message)
+    }
 }
