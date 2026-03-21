fe4acb5ece2a7fbc2eff382cc841bc4f5201b279
SamyBK07 <kpeigounousamiou@gmail.com>
2026-03-21 17:50:18 +0100

    Create WeatherAction.kt

---
+++ app/src/main/java/com/navarro/actions/WeatherAction.kt
@@ -0,0 +1,8 @@
+package com.navarro.actions
+
+class WeatherAction : BaseAction() {
+    override fun execute(command: String): String {
+        // TODO: Intégration avec une API météo ou service local
+        return "Action météo pour: $command (non implémentée)"
+    }
+}
