58ca1c14e96b7ad289891fd1576b5a15fa8bb143
SamyBK07 <kpeigounousamiou@gmail.com>
2026-03-21 04:16:10 +0100

    Update BootReceiver.kt

--- app/src/main/java/com/navarro/hotword/BootReceiver.kt
+++ app/src/main/java/com/navarro/hotword/BootReceiver.kt
@@ -3,13 +3,21 @@ package com.navarro.hotword
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import com.navarro.core.Logger
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
         if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
             Logger.d("Redémarrage après boot")
-            context.startService(Intent(context, HotwordService::class.java))
+
+            val serviceIntent = Intent(context, HotwordService::class.java)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
         }
     }
 }
